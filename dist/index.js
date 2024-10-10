a5b370555c3275c05557b1e78c17f7fb6597ce22
RidhikaJoshi <ridhikasmrh@gmail.com>
2024-10-10 19:48:13 +0530

    Add constants file with DB_NAME and add connect to MongoDB function

--- dist/index.js
+++ dist/index.js
@@ -14,24 +14,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const axios_1 = __importDefault(require("axios"));
+const ApiError_1 = require("./utils/ApiError");
+const ApiResponse_1 = require("./utils/ApiResponse");
+const asyncHandler_1 = require("./utils/asyncHandler");
 const app = (0, express_1.default)();
 // Task 1
 // Implement a background job that will fetch the current price in USD,
 //  market cap in USD and 24 hour change of 3 cryptocurrencies: Bitcoin, Matic, and Ethereum and store it in a database. 
 // This job should run once every 2 hours.
-app.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.get('/', (0, asyncHandler_1.asyncHandler)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     const response = yield axios_1.default.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cmatic-network&vs_currencies=usd&include_market_cap=true&include_24hr_vol=false&include_24hr_change=true&include_last_updated_at=false', {
         headers: { accept: 'application/json', 'x-cg-demo-api-key': process.env.COINGECKO_API_KEY }
     });
     //console.log(response.data);
     res
         .status(200)
-        .json({
-        status: 200,
-        message: "Fetched successfully",
-        data: response.data
-    });
-}));
+        .json(new ApiResponse_1.ApiResponse(200, "Data Fetched successfully", response.data));
+})));
 // Task 2
 // Implement an API /stats, that will return the latest data about the requested cryptocurrency.
 // Query params:
@@ -43,17 +42,13 @@ app.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
 // {
 // 	price: 40000,
 // 	marketCap: 800000000,
-// 	"24hChange": 3.4
-// }
-app.get('/stats', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.get('/stats', (0, asyncHandler_1.asyncHandler)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     const coin = req.query.coin;
     if (!coin || !['bitcoin', 'ethereum', 'matic-network'].includes(coin)) {
         res
             .status(400)
-            .json({
-            status: 400,
-            message: "Please provide coin name"
-        });
+            .json(new ApiError_1.ApiError(400, 'Invalid coin name'));
+        return;
     }
     const response = yield axios_1.default.get(`https://api.coingecko.com/api/v3/simple/price?ids=${coin}&vs_currencies=usd&include_market_cap=true&include_24hr_vol=false&include_24hr_change=true&include_last_updated_at=false`, {
         headers: { accept: 'application/json', 'x-cg-demo-api-key': process.env.COINGECKO_API_KEY }
@@ -61,12 +56,8 @@ app.get('/stats', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     //console.log(response.data);
     res
         .status(200)
-        .json({
-        status: 200,
-        message: "Fetched successfully",
-        data: response.data
-    });
-}));
+        .json(new ApiResponse_1.ApiResponse(200, "Data Fetched successfully", response.data));
+})));
 app.listen(8000, () => {
     console.log('Server is running on port 8000');
 });
